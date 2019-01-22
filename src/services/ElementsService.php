5d7b5a0f266bcf72b65f8194f54453a5a5a8d09f
Jan Henckens <jan@studioespresso.co>
2019-01-22 19:36:20 +0100

    Feed template

---
+++ src/services/ElementsService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace studioespresso\productsfeedfacebook\services;
+
+use craft\base\Component;
+use craft\commerce\elements\Product;
+use craft\elements\db\ElementQuery;
+
+class ElementsService extends Component
+{
+    public function getProducts(ElementQuery $query = null)
+    {
+        if ($query) {
+            $products = $query->all();
+        } else {
+            $products = Product::find()->limit(1)->all();
+        }
+        return $products;
+    }
+}
